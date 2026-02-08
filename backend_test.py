daf4f9c117ee24a17a3332214e97b253c82a1c71
Vishnuvardan <vishnuvardan.bnr@gmail.com>
2026-02-08 14:49:11 +0000

    auto-commit for d278d440-ccda-4bb3-97e0-07fbd689b1f8

--- backend_test.py
+++ backend_test.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 class GemBotAPITester:
-    def __init__(self, base_url="https://gembot-network.preview.emergentagent.com"):
+    def __init__(self, base_url="https://gem-bot-mlm.preview.emergentagent.com"):
         self.base_url = base_url
         self.api_url = f"{base_url}/api"
         self.user_token = None
